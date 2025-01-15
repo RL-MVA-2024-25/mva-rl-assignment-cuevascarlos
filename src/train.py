bb51e176fa7d5aebdab36e9ddef43aba03a6d810
cuevascarlos <carlos.cuevas-villarmin@universite-paris-saclay.fr>
2025-01-15 15:49:33 +0100

    DQN agent

--- src/train.py
+++ src/train.py
@@ -1,193 +1,203 @@
+import random
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
-# import wandb
-import pickle
-import os
+from copy import deepcopy
 from tqdm import tqdm
-import random
-import joblib
-from huggingface_hub import hf_hub_download
+#import wandb
+import matplotlib.pyplot as plt
+import os
+
+class ReplayBuffer:
+    def __init__(self, capacity, device):
+        self.capacity = int(capacity)
+        self.data = []
+        self.index = 0
+        self.device = device
+
+    def append(self, s, a, r, s_, d):
+        if len(self.data) < self.capacity:
+            self.data.append(None)
+        self.data[self.index] = (s, a, r, s_, d)
+        self.index = (self.index + 1) % self.capacity
+
+    def sample(self, batch_size):
+        batch = random.sample(self.data, batch_size)
+        return [torch.tensor(np.array(x), dtype=torch.float32, device=self.device) for x in zip(*batch)]
+
+    def __len__(self):
+        return len(self.data)
+
+def greedy_action(network, state):
+   # Ensure the device is set to match the network's parameters
+    device = next(network.parameters()).device
+    # Convert the state to a torch tensor and add a batch dimension
+    state_tensor = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    with torch.no_grad():
+        Q = network(state_tensor)
+        return torch.argmax(Q).item()
 
 class ProjectAgent:
-    def __init__(self, env=None, gamma=0.99, epsilon=1.0, epsilon_min=0.05, decay_rate=0.995, epsilon_delay_decay=50, max_buffer_size=300000):  
-        self.models = [RandomForestRegressor(n_estimators=50, random_state=42) for _ in range(4)]
-        self.state_dim = 6
-        self.action_dim = 4
-        self.scalers = [StandardScaler() for _ in range(self.action_dim)]
-        self.actions = list(range(env.action_space.n)) if env else [0, 1, 2, 3]
-        self.initialized = [False for _ in range(4)]
-        self.gamma = gamma
+    def __init__(self, config= {}, input_dim=6, action_dim=4):
+        #self.device = torch.device("cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Using device: {self.device}")
+        self.nb_actions = action_dim
+        self.gamma = config.get('gamma', 0.95)
+        self.batch_size = config.get('batch_size', 256)
+        buffer_size = config.get('buffer_size', int(1e5))
+        self.memory = ReplayBuffer(buffer_size, self.device)
+        self.epsilon_max = config.get('epsilon_max', 1.0)
+        self.epsilon_min = config.get('epsilon_min', 0.1)
+        self.epsilon_stop = config.get('epsilon_decay_period', 1000)
+        self.epsilon_delay = config.get('epsilon_delay_decay', 50)
+        self.epsilon_step = (self.epsilon_max - self.epsilon_min) / self.epsilon_stop
+        self.epsilon = self.epsilon_max
 
-        self.max_buffer_size = max_buffer_size
-        self.buffer = []
+        self.define_model(input_dim, action_dim)
+        self.target_model = deepcopy(self.model).to(self.device)
+        self.criterion = config.get('criterion', torch.nn.MSELoss())
+        lr = config.get('learning_rate', 0.001)
+        self.optimizer = config.get('optimizer', torch.optim.Adam(self.model.parameters(), lr=lr))
+        self.nb_gradient_steps = config.get('gradient_steps', 1)
+        self.update_target_strategy = config.get('update_target_strategy', 'replace')
+        self.update_target_freq = config.get('update_target_freq', 20)
+        self.update_target_tau = config.get('update_target_tau', 0.005)
+        self.target_model.load_state_dict(self.model.state_dict())
 
-        self.epsilon = epsilon
-        self.epsilon_min = epsilon_min
-        self.decay_rate = decay_rate
-        self.epsilon_delay = epsilon_delay_decay
 
-    def act(self, observation, use_random=False):
-        #if (use_random or not self.initialized) and random.random() < self.epsilon:
-        #    return random.choice(self.actions)
-        if use_random and random.random() < self.epsilon:
-            return random.randint(0, self.action_dim - 1)
-
-        q_values = []
-        for a in self.actions:
-            try:
-                obs_scaled = self.scalers[a].transform(np.hstack([observation]).reshape(1, -1))
-                q_value = self.models[a].predict(obs_scaled)[0]
-            except:
-                q_value = float('-inf')
-            q_values.append(q_value)
-        
-        return int(np.argmax(q_values))
-    
-    def set_epsilon(self):
-        self.epsilon = max(self.epsilon_min, self.epsilon * self.decay_rate)
-
-    def collect_transitions(self, steps, env, use_random=True):
-        transitions = []
+    def fill_buffer(self, env, num_steps):
         state, _ = env.reset()
-        
-        for _ in tqdm(range(steps)):
-            action = self.act(state, use_random)
+        for _ in tqdm(range(num_steps), desc="Filling buffer"):
+            action = env.action_space.sample()
             next_state, reward, done, trunc, _ = env.step(action)
-
-            transitions.append((state, action, reward, next_state, done))
-
+            self.memory.append(state, action, reward, next_state, done)
             if done or trunc:
                 state, _ = env.reset()
+                print(f"Done: {done}, Trunc: {trunc}")
             else:
                 state = next_state
-                
-        return transitions
 
-    def _prepare_fqi_dataset(self, transitions):
-        states = np.vstack([t[0] for t in transitions])
-        actions = np.array([t[1] for t in transitions])
-        rewards = np.array([t[2] for t in transitions])
-        next_states = np.vstack([t[3] for t in transitions])
-        dones = np.array([t[4] for t in transitions])
+    def act(self, observation, use_random=False):
+        if use_random and random.random() < self.epsilon:
+            return random.randint(0, self.nb_actions - 1)
+        return greedy_action(self.model, observation)
 
-        action_datasets = [[] for _ in range(self.action_dim)]
-        action_targets = [[] for _ in range(self.action_dim)]
+    def save(self, path):
+        torch.save(self.model.state_dict(), path)
 
-        next_q_values = np.zeros((len(states), self.action_dim))
-        if self.initialized[0]:
-            for a in range(self.action_dim):
-                next_states_scaled = self.scalers[a].transform(next_states)
-                next_q_values[:, a] = self.models[a].predict(next_states_scaled)
+    def load(self, path="./DQN_model.pth"):
+        self.model.load_state_dict(torch.load(path, map_location=self.device))
+        self.target_model.load_state_dict(self.model.state_dict())
 
-        max_next_q = np.max(next_q_values, axis=1)
+    def gradient_step(self):
+        if len(self.memory) > self.batch_size:
+            X, A, R, Y, D = self.memory.sample(self.batch_size)
+            QYmax = self.target_model(Y).max(1)[0].detach()
+            update = torch.addcmul(R, 1 - D, QYmax, value=self.gamma)
+            QXA = self.model(X).gather(1, A.to(torch.long).unsqueeze(1))
+            loss = self.criterion(QXA, update.unsqueeze(1))
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
 
-        for i in range(len(states)):
-            action = int(actions[i])
-            target = rewards[i]
-            if not dones[i]:
-                target += self.gamma * max_next_q[i]
-
-            action_datasets[action].append(states[i])
-            action_targets[action].append(target)
-
-        return action_datasets, action_targets
-
-    def evaluate(self, env, num_episodes=5):
-        total_reward = 0
-        for _ in tqdm(range(num_episodes), desc="Evaluating"):
+    def train(self, env, max_episode):
+        episode_return = []
+        state, _ = env.reset()
+        best_reward = float("-inf")
+        last_update_best_reward = float("-inf")
+        step = 0
+        flag_env = "NO_RANDOM"
+        for episode in range(max_episode):
+            episode_cum_reward = 0
             state, _ = env.reset()
-            done = False
-            trunc = False
-            while not done and not trunc:
-                action = self.act(state, use_random=False)
-                next_state, reward, done, trunc, _ = env.step(action)
-                total_reward += reward
-                state = next_state
-        return total_reward / num_episodes
+            with tqdm(total=200, desc=f"Episode {episode + 1} - {flag_env}", unit="step") as pbar:
+                while True:
+                    if step > self.epsilon_delay:
+                        self.epsilon = max(self.epsilon_min, self.epsilon - self.epsilon_step)
 
-    def save(self, name, path = None):
-        if path is None:
-            path = f"./{name}.pkl"
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        save_dict = {
-            'models': self.models,
-            'scalers': self.scalers
-        }
-        joblib.dump(save_dict, path)
+                    action = self.act(state, use_random=True)
 
-    def load(self, name="RF_model_rounded", path = None):  
-        model_path = hf_hub_download(repo_id="cuevascarlos/HIV-RL-RandomForest-policy", filename = "RF_model.pkl")
+                    next_state, reward, done, trunc, _ = env.step(action)
+                    self.memory.append(state, action, reward, next_state, done)
+                    episode_cum_reward += reward
 
-        if path is None:
-            path = f"./{name}.pkl"
-        save_dict = joblib.load(model_path)
-        self.models = save_dict['models']
-        self.scalers = save_dict['scalers']
-        self.initialized = [True for _ in range(4)]
+                    for _ in range(self.nb_gradient_steps):
+                        self.gradient_step()
 
-    def fit_models(self, action_datasets, action_targets):
-        for a in range(self.action_dim):
-            print(f"Training model {a} with {len(action_datasets[a])} samples")
-            if len(action_datasets[a]) > 0:
-                X = np.array(action_datasets[a])
-                y = np.array(action_targets[a])
+                    if self.update_target_strategy == 'replace':
+                        if step % self.update_target_freq == 0 and not episode_cum_reward*10 < last_update_best_reward: 
+                            last_update_best_reward = episode_cum_reward
+                            self.target_model.load_state_dict(self.model.state_dict())
+                    elif self.update_target_strategy == 'ema':
+                        target_state_dict = self.target_model.state_dict()
+                        model_state_dict = self.model.state_dict()
+                        for key in model_state_dict:
+                            target_state_dict[key] = self.update_target_tau * model_state_dict[key] + (1 - self.update_target_tau) * target_state_dict[key]
+                        self.target_model.load_state_dict(target_state_dict)
 
-                X_scaled = self.scalers[a].fit_transform(X)
-                self.models[a].fit(X_scaled, y)
-                self.initialized[a] = True
-
-    def bootstrap_creation(self):
-        bootstrap_idx = np.random.choice(len(self.buffer), len(self.buffer), replace=True)
-        bootstrap_transitions = [self.buffer[i] for i in bootstrap_idx]
-        action_datasets, action_targets = self._prepare_fqi_dataset(bootstrap_transitions)
-        return action_datasets, action_targets
-
-    def train(self, env, num_epochs=100, episodes_per_epoch=100, initialize_buffer=False):
-
-        if initialize_buffer:
-            self.buffer = self.collect_transitions(self.max_buffer_size, env, use_random=True)
-            # Fit models
-            action_datasets, action_targets = self.bootstrap_creation()
-            self.fit_models(action_datasets, action_targets)
-            agent.set_epsilon()
+                    step += 1
+                    pbar.update(1)
+                    if done or trunc:
+                        episode_return.append(episode_cum_reward)
+                        #wandb.log({"Episode": episode + 1, "Return": episode_cum_reward, "Epsilon": self.epsilon})
+                        if episode_cum_reward > best_reward:
+                            best_reward = episode_cum_reward
+                            self.save(f"./dqn_best.pth")
+                        if episode % 50 == 0 and episode > 0:
+                            if random.random() < 1.0:
+                                env = TimeLimit(env=HIVPatient(domain_randomization=True), max_episode_steps=200)
+                                flag_env = "RANDOM"
+                            else:
+                                env = TimeLimit(env=HIVPatient(domain_randomization=False), max_episode_steps=200)
+                                flag_env = "NO_RANDOM"
+                        elif episode % 10 == 0:
+                            env = TimeLimit(env=HIVPatient(domain_randomization=False), max_episode_steps=200)
+                            flag_env = "NO_RANDOM"
+                        break
+                    else:
+                        state = next_state
+                    
+            print(f"Episode {episode + 1} - Reward: {"{:.3e}".format(episode_cum_reward)} - Epsilon: {self.epsilon:.4f}")
+            # Save model every 100 episodes
+            if episode_cum_reward > 1e10:
+                self.save(f"./models_dqn/dqn_{episode}_high.pth")
             
-        best_eval_reward = float('-inf')
+        return episode_return
 
-        for epoch in range(num_epochs):
-            epoch_rewards = []
-
-            for episode in tqdm(range(episodes_per_epoch), desc=f"Epoch {epoch + 1}/{num_epochs}"):
-                episode_transitions = self.collect_transitions(200, env, use_random=False)
-                episode_reward = sum(t[2] for t in episode_transitions)
-                self.buffer.extend(episode_transitions)
-                if len(self.buffer) > self.max_buffer_size:
-                    self.buffer = self.buffer[-self.max_buffer_size:]
-                epoch_rewards.append(episode_reward)
-                agent.set_epsilon()
-
-            action_datasets, action_targets = self.bootstrap_creation()
-            self.fit_models(action_datasets, action_targets)
-
-            eval_reward = self.evaluate(env, num_episodes=10)
-            avg_reward = np.mean(epoch_rewards)
-
-            print(f"Epoch {epoch + 1}  | Summary: Avg Epoch Reward: {avg_reward:.2e}, Eval Reward: {eval_reward:.2e}, Epsilon: {self.epsilon:.2e}")
-            # wandb.log({"epoch": epoch, "avg_reward": avg_reward, "eval_reward": eval_reward})
-
-            # self.save(f"model_{epoch+1}")
-            if eval_reward > best_eval_reward:
-                best_eval_reward = eval_reward
-                print(f"Best eval reward: {epoch+1} - Value: {best_eval_reward}")
-                self.save(f"best_eval_model")
-        
-        self.save("final_model")
+    def define_model(self, input_dim, output_dim):
+        self.model = nn.Sequential(
+        nn.Linear(input_dim, 256),
+        nn.ReLU(),
+        nn.Linear(256, 256),
+        nn.ReLU(),
+        nn.Linear(256, 256),
+        nn.ReLU(),
+        nn.Linear(256,output_dim)
+        ).to(self.device)
 
 if __name__ == "__main__":
-    # wandb.init(project="FQI-HIV", name="combined_training")
-    env = TimeLimit(env=HIVPatient(domain_randomization=True), max_episode_steps=200)
-    agent = ProjectAgent()
-    agent.train(env, num_epochs=100, episodes_per_epoch=100, initialize_buffer=True)
-    # wandb.finish()
+    env = TimeLimit(env=HIVPatient(domain_randomization=False), max_episode_steps=200)
+    config = {
+        'gamma': 0.97,
+        'batch_size': 800,
+        'buffer_size': int(1e5),
+        'epsilon_max': 1.0,
+        'epsilon_min': 0.05,
+        'epsilon_decay_period': 1000,
+        'epsilon_delay_decay': 500,
+        'learning_rate': 1e-3,
+        'gradient_steps': 4,
+        'update_target_strategy': 'replace',
+        'update_target_freq': 400,
+        'update_target_tau': 0.005,
+        'criterion': torch.nn.SmoothL1Loss(),
+    }
+    #wandb.init(project="FQI-HIV", name="DQN-1")
+    agent = ProjectAgent(config=config, input_dim=6, action_dim=4)
+    #agent.fill_buffer(env, num_steps=100000)  # Pre-fill buffer
+    episode_return = agent.train(env, max_episode=1000)
+    agent.save(f"./models_dqn/dqn_final.pth")
